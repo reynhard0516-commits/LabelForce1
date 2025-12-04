a00432da954e3899b45894a30b16bef457e18c6e
reynhard0516-commits <reynhard0516@gmail.com>
2025-12-05 00:29:19 +0200

    Update security.py

--- Backend/app/utils/security.py
+++ Backend/app/utils/security.py
@@ -1,1 +1,20 @@
 
+from jose import jwt
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+from app.config import settings
+
+pwd = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hash_password(password: str):
+    return pwd.hash(password)
+
+def verify_password(password: str, hash: str):
+    return pwd.verify(password, hash)
+
+def create_token(user_id: int):
+    payload = {
+        "user_id": user_id,
+        "exp": datetime.utcnow() + timedelta(days=7)
+    }
+    return jwt.encode(payload, settings.JWT_SECRET, settings.JWT_ALG)

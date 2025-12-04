93212fa3d399a3c49293a3a2c3813699ad826e8d
reynhard0516-commits <reynhard0516@gmail.com>
2025-12-05 00:25:19 +0200

    Add configuration settings with environment variables

--- Backend/app/config.py
+++ Backend/app/config.py
@@ -1,1 +1,14 @@
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+class Settings:
+    PROJECT_NAME = "Labelforce Backend"
+    DATABASE_URL = os.getenv("DATABASE_URL")
+    JWT_SECRET = os.getenv("JWT_SECRET")
+    JWT_ALG = "HS256"
+    REDIS_URL = os.getenv("REDIS_URL")
+
+settings = Settings()

918ca0c66d16b3016a08da672c136f139d014096
reynhard0516-commits <reynhard0516@gmail.com>
2025-12-05 00:27:32 +0200

    Update label.py

--- Backend/app/models/label.py
+++ Backend/app/models/label.py
@@ -1,1 +1,12 @@
 
+from sqlalchemy import Column, Integer, String, Boolean
+from app.database import Base
+
+class Label(Base):
+    __tablename__ = "labels"
+
+    id = Column(Integer, primary_key=True)
+    sku = Column(String, unique=True)
+    product_name = Column(String)
+    enabled = Column(Boolean, default=True)
+    group_name = Column(String, nullable=True)

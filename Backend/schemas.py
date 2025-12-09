022303fbd5ed2c433fb96519bd1a1cb54532fd20
reynhard0516-commits <reynhard0516@gmail.com>
2025-12-09 21:12:32 +0200

    Update schemas.py

--- Backend/schemas.py
+++ Backend/schemas.py
@@ -1,30 +1,12 @@
 from pydantic import BaseModel
-from typing import Optional
 
 class UserCreate(BaseModel):
     email: str
     password: str
 
+class Token(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
 
-class UserLogin(BaseModel):
-    email: str
-    password: str
-
-
-class UserOut(BaseModel):
-    id: int
-    email: str
-    is_admin: bool
-
-    class Config:
-        orm_mode = True
-
-
-class ProjectCreate(BaseModel):
-    name: str
-    description: Optional[str]
-
-
-class TaskCreate(BaseModel):
-    project_id: int
-    file_path: str
+class TokenData(BaseModel):
+    sub: str | None = None

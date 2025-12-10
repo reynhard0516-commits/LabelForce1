eb99e35d68908b6afce76b09f54beece142e5b66
reynhard0516-commits <reynhard0516@gmail.com>
2025-12-10 10:58:36 +0200

    Update models.py

--- Backend/models.py
+++ Backend/models.py
@@ -1,8 +1,11 @@
-from sqlmodel import SQLModel, Field
 from typing import Optional
+from datetime import datetime
+from sqlmodel import SQLModel, Field
+
 
 class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    email: str = Field(index=True, unique=True, nullable=False)
+    email: str = Field(index=True, unique=True)
     hashed_password: str
     is_admin: bool = Field(default=False)
+    created_at: datetime = Field(default_factory=datetime.utcnow)

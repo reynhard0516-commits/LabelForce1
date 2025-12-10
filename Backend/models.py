51b4524d39ed8005eccb5d3b9730d670da1d7b84
reynhard0516-commits <reynhard0516@gmail.com>
2025-12-10 14:41:05 +0200

    Update models.py

--- Backend/models.py
+++ Backend/models.py
@@ -3,6 +3,7 @@ from sqlmodel import SQLModel, Field
 from typing import Optional
 from datetime import datetime
 
+
 class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     email: str = Field(index=True, nullable=False, unique=True)

935e48fea60dac37fb948e3d6dffa0bbf6495b07
reynhard0516-commits <reynhard0516@gmail.com>
2025-12-10 10:27:43 +0200

    Update database.py

--- Backend/database.py
+++ Backend/database.py
@@ -1,20 +1,16 @@
 from sqlmodel import SQLModel
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.ext.asyncio import async_sessionmaker
 from config import settings
 
-DATABASE_URL = settings.DATABASE_URL
-
-# Async engine + sessionmaker
-engine = create_async_engine(DATABASE_URL, echo=False, future=True)
+engine = create_async_engine(settings.DATABASE_URL, echo=False, future=True)
 AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
 async def create_db_and_tables():
-    """Create DB tables (call at startup)."""
     async with engine.begin() as conn:
         await conn.run_sync(SQLModel.metadata.create_all)
 
 async def get_session():
-    """Dependency: async generator yielding a session."""
     async with AsyncSessionLocal() as session:
         yield session

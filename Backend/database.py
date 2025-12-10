9143cd63ccf91612fd22daf2de4a4a41c948375e
reynhard0516-commits <reynhard0516@gmail.com>
2025-12-10 15:49:22 +0200

    Update database.py

--- Backend/database.py
+++ Backend/database.py
@@ -1,9 +1,8 @@
 import os
-from sqlmodel import SQLModel
-from sqlmodel.ext.asyncio.session import AsyncSession
-from sqlalchemy.ext.asyncio import create_async_engine
-from sqlalchemy.orm import sessionmaker
-from Backend.config import DATABASE_URL
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_async_engine(
     DATABASE_URL,
@@ -13,18 +12,16 @@ engine = create_async_engine(
 
 async_session = sessionmaker(
     engine,
-    class_=AsyncSession,
-    expire_on_commit=False
+    expire_on_commit=False,
+    class_=AsyncSession
 )
+
+Base = declarative_base()
 
 async def get_session():
     async with async_session() as session:
         yield session
 
-async def init_db(drop_first=False):
-    from Backend import models
-
+async def init_db():
     async with engine.begin() as conn:
-        if drop_first:
-            await conn.run_sync(SQLModel.metadata.drop_all)
-        await conn.run_sync(SQLModel.metadata.create_all)
+        await conn.run_sync(Base.metadata.create_all)

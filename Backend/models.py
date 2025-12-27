d221e4e0f270d35068c9794c6409014d54485ee9
reynhard0516-commits <reynhard0516@gmail.com>
2025-12-27 02:10:31 +0200

    Update models.py

--- Backend/models.py
+++ Backend/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import String, Integer
+from sqlalchemy import Integer, String
 
 class Base(DeclarativeBase):
     pass

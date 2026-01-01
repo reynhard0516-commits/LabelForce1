7dead47fcbedc51b8202d048ba9d9515526cf0df
reynhard0516-commits <reynhard0516@gmail.com>
2026-01-01 12:20:07 +0200

    Update models.py

--- Backend/models.py
+++ Backend/models.py
@@ -1,12 +1,9 @@
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Integer, String
-
-class Base(DeclarativeBase):
-    pass
+from sqlalchemy import Column, Integer, String
 
 class User(Base):
     __tablename__ = "users"
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    email: Mapped[str] = mapped_column(String, unique=True, index=True)
-    password: Mapped[str] = mapped_column(String)
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    password = Column(String)
+    role = Column(String, default="user")  # 👈 ADD THIS

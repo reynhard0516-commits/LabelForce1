5f1fb488e8e1fa9ebcc737c0ae16fb41d5e42367
reynhard0516-commits <reynhard0516@gmail.com>
2026-01-01 10:05:49 +0200

    Update auth.py

--- Backend/auth.py
+++ Backend/auth.py
@@ -3,22 +3,21 @@ from jose import jwt
 from passlib.context import CryptContext
 import os
 
-# PASSWORD CONTEXT
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-# SECURITY SETTINGS
-SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-change-me")
+SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
-# PASSWORD HELPERS
+
 def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
+
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
-# JWT TOKEN
+
 def create_access_token(data: dict):
     to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)

58325bdecb13d25753ace36f91033a519126963f
reynhard0516-commits <reynhard0516@gmail.com>
2026-01-01 23:59:58 +0200

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,6 +1,14 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
 
-const container = document.getElementById("root");
-createRoot(container).render(<App />);
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <>
+      <Login />
+      <Profile />
+    </>
+  </React.StrictMode>
+);

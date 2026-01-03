847ba6a0e0a057d00cebeb867e1cc0328613aa00
reynhard0516-commits <reynhard0516@gmail.com>
2026-01-03 19:40:37 +0200

    Create user.py

---
+++ Backend/models/user.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+from database import Base
+from sqlalchemy.orm import relationship
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
+
+    datasets = relationship("Dataset", back_populates="owner")

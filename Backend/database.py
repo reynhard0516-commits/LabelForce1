2d08f1a2a5fc8a9f1b7ce6053bfce474a14768dd
reynhard0516-commits <reynhard0516@gmail.com>
2026-01-03 19:39:18 +0200

    Update database.py

--- Backend/database.py
+++ Backend/database.py
@@ -1,21 +1,18 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker
-from models import Base
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.environ["DATABASE_URL"]
 
-engine = create_async_engine(
-    DATABASE_URL,
-    echo=False
-)
+engine = create_async_engine(DATABASE_URL, echo=False)
 
 AsyncSessionLocal = sessionmaker(
-    bind=engine,
-    class_=AsyncSession,
-    expire_on_commit=False
+    engine, class_=AsyncSession, expire_on_commit=False
 )
 
-async def get_session():
+class Base(DeclarativeBase):
+    pass
+
+async def get_db():
     async with AsyncSessionLocal() as session:
         yield session

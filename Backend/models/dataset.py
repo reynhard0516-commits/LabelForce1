c933a86390ba2c9549ea95d99f19c3d23f64d135
reynhard0516-commits <reynhard0516@gmail.com>
2026-01-03 21:56:06 +0200

    Update dataset.py

--- Backend/models/dataset.py
+++ Backend/models/dataset.py
@@ -1,10 +1,11 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from database import Base
 
+
 class Dataset(Base):
     __tablename__ = "datasets"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     description = Column(String)
     owner_id = Column(Integer, ForeignKey("users.id"), nullable=False)

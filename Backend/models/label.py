2c5ef92232796ef53d8edda13cd6a2e9733f2b8c
reynhard0516-commits <reynhard0516@gmail.com>
2026-01-03 22:18:30 +0200

    Update label.py

--- Backend/models/label.py
+++ Backend/models/label.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from database import Base
 
 class Label(Base):
@@ -7,3 +8,6 @@ class Label(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     dataset_id = Column(Integer, ForeignKey("datasets.id"))
+
+    dataset = relationship("Dataset", back_populates="labels")
+    annotations = relationship("Annotation", back_populates="label")

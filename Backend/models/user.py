bdd1aadb2f00edad45f7ed0b5ddff42531f6bfc0
reynhard0516-commits <reynhard0516@gmail.com>
2026-01-03 19:54:14 +0200

    Update user.py

--- Backend/models/user.py
+++ Backend/models/user.py
@@ -1,12 +1,9 @@
 from sqlalchemy import Column, Integer, String
-from database import Base
-from sqlalchemy.orm import relationship
+from Backend.database import Base
 
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True)
-    email = Column(String, unique=True, nullable=False)
-    password = Column(String, nullable=False)
-
-    datasets = relationship("Dataset", back_populates="owner")
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)

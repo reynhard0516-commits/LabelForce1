379dfd816e3f2b2b495cdaedb2eeba8ae081b674
reynhard0516-commits <reynhard0516@gmail.com>
2026-01-03 21:55:29 +0200

    Update user.py

--- Backend/models/user.py
+++ Backend/models/user.py
@@ -1,9 +1,10 @@
 from sqlalchemy import Column, Integer, String
 from database import Base
 
+
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True)
     email = Column(String, unique=True, index=True, nullable=False)
-    hashed_password = Column(String, nullable=False)
+    password = Column(String, nullable=False)

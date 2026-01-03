20d457bd459c2d924fe57b75f4035b8b84330d14
reynhard0516-commits <reynhard0516@gmail.com>
2026-01-03 19:54:51 +0200

    Update dataset.py

--- Backend/models/dataset.py
+++ Backend/models/dataset.py
@@ -1,14 +1,10 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
-from database import Base
+from Backend.database import Base
 
 class Dataset(Base):
     __tablename__ = "datasets"
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
     description = Column(String)
-    owner_id = Column(Integer, ForeignKey("users.id"))
-
-    owner = relationship("User", back_populates="datasets")
-    items = relationship("DataItem", cascade="all, delete")
+    owner_id = Column(Integer, ForeignKey("users.id"), nullable=False)

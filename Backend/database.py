70b4eab3799ab2c8e8a71c6dbdb1949016a86125
reynhard0516-commits <reynhard0516@gmail.com>
2026-01-04 08:03:08 +0200

    Rename get_db function to get_session

--- Backend/database.py
+++ Backend/database.py
@@ -1,10 +1,10 @@
+import os
 from sqlalchemy.ext.asyncio import (
     AsyncSession,
     create_async_engine,
     async_sessionmaker,
 )
 from sqlalchemy.orm import declarative_base
-import os
 
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
@@ -23,7 +23,6 @@ AsyncSessionLocal = async_sessionmaker(
 
 Base = declarative_base()
 
-
-async def get_db() -> AsyncSession:
+async def get_session() -> AsyncSession:
     async with AsyncSessionLocal() as session:
         yield session

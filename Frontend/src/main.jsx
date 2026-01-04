32e3e0c260a6b2b2b9de19af0e66762101cd2b50
reynhard0516-commits <reynhard0516@gmail.com>
2026-01-04 07:49:07 +0200

    Implement routing with protected routes in main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Datasets from "./pages/Datasets";
+import DatasetDetail from "./pages/DatasetDetail";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Datasets />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/datasets/:id"
+          element={
+            <ProtectedRoute>
+              <DatasetDetail />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );

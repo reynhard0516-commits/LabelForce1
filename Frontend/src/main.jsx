c1e56239ba1a830052b35f909b4cce9b291e8b1b
reynhard0516-commits <reynhard0516@gmail.com>
2026-01-04 07:25:47 +0200

    Refactor main.jsx to use App component

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,45 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-
-import Login from "./pages/Login";
-import Datasets from "./pages/Datasets";
-import DatasetDetail from "./pages/DatasetDetail";
-import { isLoggedIn } from "./services/auth";
-
-function ProtectedRoute({ children }) {
-  if (!isLoggedIn()) {
-    return <Navigate to="/login" replace />;
-  }
-  return children;
-}
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-
-        <Route
-          path="/"
-          element={
-            <ProtectedRoute>
-              <Datasets />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/datasets/:id"
-          element={
-            <ProtectedRoute>
-              <DatasetDetail />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
